338dc19dca99c4fbbb3ffd425b24498e9802634a
kynkcaycan <105385269+kynkcaycan@users.noreply.github.com>
2025-03-29 03:14:14 +0300

    third commit

--- polls/models.py
+++ polls/models.py
@@ -10,8 +10,12 @@ class Question(models.Model):
     #models.ForeignKey() veri tipi
     #models.CASCADE() veri tipi
     pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.question_text
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
